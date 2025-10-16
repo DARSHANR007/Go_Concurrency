ccf8dd95f4aa9de91c886d1ca7e4791ae8d99e47
darsh <darshanravichandran07@gmail.com>
2025-10-16 11:09:33 +0530

    added channel

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Print("this is the start")
+
+	fmt.Println("ths is the start ")
+
+	now := time.Now()
+
+	userID := 10
+
+	getUserId(userID)
+	getUserLikes(userID)
+	getUserPosts(userID)
+
+	fmt.Println(time.Since(now))
+
+}
+
+func getUserId(id int) {
+
+	time.Sleep(120 * time.Millisecond)
+
+}
+
+func getUserLikes(id int) {
+	time.Sleep(80 * time.Millisecond)
+}
+
+func getUserPosts(id int) {
+	time.Sleep(50 * time.Millisecond)
 }
